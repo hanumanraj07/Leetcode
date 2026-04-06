65cfc1e1e2602858ce63606b27eaf14bd26a18a8
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 18:52:31 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.c
@@ -0,0 +1,14 @@
+// Last updated: 4/6/2026, 6:52:31 PM
+bool isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0))
+        return false;
+
+    int reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+    
+    return (x == reversedHalf || x == reversedHalf / 10);
+}
