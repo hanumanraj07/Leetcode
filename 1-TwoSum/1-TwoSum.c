b9a04a983d65b50fe1aec16508103c860dfb50a1
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 18:52:35 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.c
@@ -0,0 +1,18 @@
+// Last updated: 4/6/2026, 6:52:34 PM
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
