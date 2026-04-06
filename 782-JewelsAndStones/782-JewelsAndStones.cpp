d400743031f7fd59d8dc953e3dd3f17c7f524257
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 17:56:15 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/6/2026, 5:56:14 PM
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+
+        unordered_set<char> s(jewels.begin(), jewels.end());
+        int count = 0;
+
+        for(char c : stones){
+            if(s.count(c))
+                count++;
+        }
+
+        return count;
+    }
+};
