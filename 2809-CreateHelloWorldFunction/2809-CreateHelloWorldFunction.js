a9f6d386dc6a14323fc551ef895622538ad6bb7b
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 17:55:34 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,17 @@
+// Last updated: 4/6/2026, 5:55:33 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+
+            return "Hello World";
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
