ea0926c58453a557a4218f831f94438e0b09d93a
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 17:56:02 +0530

    Create 1584-AverageSalaryExcludingTheMinimumAndMaximumSalary

---
+++ 1584-AverageSalaryExcludingTheMinimumAndMaximumSalary/1584-AverageSalaryExcludingTheMinimumAndMaximumSalary.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/6/2026, 5:56:01 PM
+class Solution {
+public:
+    double average(vector<int>& salary) {
+
+        int minSalary = INT_MAX;
+        int maxSalary = INT_MIN;
+        int sum = 0;
+
+        for(int s : salary){
+            sum += s;
+            minSalary = min(minSalary, s);
+            maxSalary = max(maxSalary, s);
+        }
+
+        return (double)(sum - minSalary - maxSalary) / (salary.size() - 2);
+    }
+};
