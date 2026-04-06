babeff739a1e5ef4b3a821e9359ef3f52fce1b29
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 17:56:27 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/6/2026, 5:56:27 PM
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+        vector<int> freq(26,0);
+
+        for(char c : s)
+            freq[c - 'a']++;
+
+        for(int i = 0; i < s.size(); i++){
+            if(freq[s[i] - 'a'] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+};
