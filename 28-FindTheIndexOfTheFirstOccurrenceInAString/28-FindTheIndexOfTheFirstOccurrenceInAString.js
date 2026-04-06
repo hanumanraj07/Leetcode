d8eddfd42e5177538f1ff2a1ea67afed63fe294e
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 18:52:27 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.js
@@ -0,0 +1,9 @@
+// Last updated: 4/6/2026, 6:52:26 PM
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+     return haystack.indexOf(needle);
+};
