e9d57f08b40c4187c31357e4dc4669088f6dc910
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 18:52:09 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/6/2026, 6:52:08 PM
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while(n != 1 && seen.count(n) == 0) {
+            seen.insert(n);
+
+            int sum = 0;
+            while(n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+};
