913553c6411f8c7732732d3dba2a297e95a0e67d
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 18:52:20 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.js
@@ -0,0 +1,21 @@
+// Last updated: 4/6/2026, 6:52:19 PM
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+      let result = [];
+
+    function backtrack(start, path) {
+        result.push([...path]);    
+        
+        for (let i = start; i < nums.length; i++) {
+            path.push(nums[i]);      
+            backtrack(i + 1, path);  
+            path.pop();              
+        }
+    }
+
+    backtrack(0, []);
+    return result;
+};
