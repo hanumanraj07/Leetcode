d5ba273dde55c39c309bced14b4faed0b845ae5f
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 17:56:18 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/6/2026, 5:56:18 PM
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0;
+
+        for (int i = 0; i <= s.length(); i++) {
+            if (i == s.length() || s[i] == ' ') {
+                reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
+        }
+
+        return s;
+    }
+};
