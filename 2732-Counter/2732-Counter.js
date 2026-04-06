72b86f211529d73469b7666bda1a557d81e9249d
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-06 17:55:37 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,17 @@
+// Last updated: 4/6/2026, 5:55:36 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+}; 
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
