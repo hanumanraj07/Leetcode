1b0e5845bb12ea2288a425e2da0db8376343e74b
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-21 15:26:34 +0530

    Create 777-ToeplitzMatrix

---
+++ 777-ToeplitzMatrix/777-ToeplitzMatrix.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/21/2026, 3:26:34 PM
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i-1][j-1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
