6f9ad8306110d22b7e19542cb593d0ab4850858d
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-03 21:14:48 +0530

    File updated at 5/3/2026, 9:14:47 PM

--- 371-SumofTwoIntegers/371-SumofTwoIntegers.cpp
+++ 371-SumofTwoIntegers/371-SumofTwoIntegers.cpp
@@ -1,9 +1,9 @@
-// Last updated: 4/19/2026, 4:51:43 PM
+// Last updated: 5/3/2026, 9:14:47 PM
 1class Solution {
 2public:
 3    int getSum(int a, int b) {
-4        while (b != 0) {
-5            int carry = (a & b) << 1;
+4        while(b != 0){
+5            unsigned int carry = (unsigned int)( a & b) << 1;
 6            a = a ^ b;
 7            b = carry;
 8        }
