be58d7e773d8b54a735085af2cb3653ab66c3f04
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-06 10:23:39 +0530

    File updated at 5/6/2026, 10:23:38 AM

--- 3024-TypeofTriangle/3024-TypeofTriangle.cpp
+++ 3024-TypeofTriangle/3024-TypeofTriangle.cpp
@@ -1,22 +1,14 @@
-// Last updated: 5/6/2026, 10:12:38 AM
+// Last updated: 5/6/2026, 10:23:38 AM
 1class Solution {
 2public:
-3    string triangleType(vector<int>& nums) {
-4        set <int> s(nums.begin(), nums.end());
+3    vector<int> findWordsContaining(vector<string>& words, char x) {
+4        vector<int> ans;
 5
-6        sort(nums.begin(), nums.end());
-7
-8        if((nums[0]+ nums[1]) <= nums[2]){
-9            return "none";
+6        for(int i = 0; i < words.size(); i++){
+7            if(words[i].find(x) != string::npos){
+8                ans.push_back(i);
+9            }
 10        }
-11
-12        if(s.size() == 1)
-13        return "equilateral";
-14
-15        if(s.size() == 2)
-16        return "isosceles";
-17
-18        return "scalene";
-19
-20         }
-21};
+11        return ans;
+12    }
+13};
