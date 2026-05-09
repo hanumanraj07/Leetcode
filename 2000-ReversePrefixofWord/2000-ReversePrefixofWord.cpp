7937950b731edb251d048ae04276106512ccb937
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-09 13:39:58 +0530

    File updated at 5/9/2026, 1:39:58 PM

--- 2000-ReversePrefixofWord/2000-ReversePrefixofWord.cpp
+++ 2000-ReversePrefixofWord/2000-ReversePrefixofWord.cpp
@@ -1,13 +1,18 @@
-// Last updated: 5/9/2026, 1:26:57 PM
+// Last updated: 5/9/2026, 1:39:58 PM
 1class Solution {
 2public:
-3    string reversePrefix(string word, char ch) {
-4        int idx = word.find(ch);
-5
-6        if(idx != string::npos){
-7            reverse(word.begin(), word.begin() + idx + 1);
-8        }
-9
-10        return word;
-11    }
-12};
+3    int countAsterisks(string s) {
+4        int count = 0;
+5        bool insidePipe = false;
+6
+7        for(char ch:s){
+8            if(ch == '|'){
+9                insidePipe = !insidePipe ;
+10            }
+11            else if(ch == '*' && !insidePipe){
+12                count++;
+13            }
+14        }
+15        return count;
+16    }
+17};
