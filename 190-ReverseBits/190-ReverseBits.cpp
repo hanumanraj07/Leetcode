520cea9d0e78a3c12704a4364868c51d960076a2
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-11 15:07:28 +0530

    File updated at 5/11/2026, 3:07:28 PM

--- 190-ReverseBits/190-ReverseBits.cpp
+++ 190-ReverseBits/190-ReverseBits.cpp
@@ -1,14 +1,13 @@
-// Last updated: 5/11/2026, 2:53:06 PM
+// Last updated: 5/11/2026, 3:07:28 PM
 1class Solution {
 2public:
 3    int reverseBits(int n) {
-4        int ans = 0;
+4        int result = 0;
 5
-6        for(int i = 0; i < 32; i++) {
-7            ans <<= 1;         
-8            ans |= (n & 1);  
-9            n >>= 1;         
-10        }
-11        return ans;
-12    }
-13};
+6        for(int i = 0 ; i < 32; i++){
+7            result = (result << 1) | (n & 1);
+8            n = n >> 1;
+9        }
+10        return result;
+11    }
+12};
