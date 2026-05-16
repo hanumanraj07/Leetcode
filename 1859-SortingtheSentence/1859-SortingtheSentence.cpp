ae202ba9adba2ade4104017b2b7122d9a1d9aa9e
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-16 10:43:18 +0530

    File updated at 5/16/2026, 10:43:19 AM

--- 1859-SortingtheSentence/1859-SortingtheSentence.cpp
+++ 1859-SortingtheSentence/1859-SortingtheSentence.cpp
@@ -1,27 +1,21 @@
-// Last updated: 5/16/2026, 10:33:41 AM
+// Last updated: 5/16/2026, 10:43:19 AM
 1class Solution {
 2public:
-3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-4        int rows = matrix.size();
-5        int cols = matrix[0].size();
+3    int findMin(vector<int>& nums) {
+4        int left = 0;
+5        int right = nums.size()-1;
 6
-7        int left = 0;
-8        int right = rows * cols -1;
+7        while(left < right){
+8            int mid = left + (right - left)/2;
 9
-10        while(left <= right){
-11            int mid = left + (right - left) / 2;
-12            int value = matrix[mid/cols][mid % cols];
-13
-14            if(value == target)
-15            return true;
-16
-17            else if(value < target)
-18            left = mid + 1;
-19
-20            else 
-21                right = mid - 1;
-22        }
-23
-24        return false;
-25    }
-26};
+10            if(nums[mid] > nums[right]){
+11                left = mid +1;
+12
+13            }
+14            else {
+15                right = mid;
+16            }
+17        }
+18        return nums[left];
+19    }
+20};
