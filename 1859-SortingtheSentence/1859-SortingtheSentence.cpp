94390675dec40dfaeea62ac27f1d4412a0ad0143
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-16 10:33:41 +0530

    File updated at 5/16/2026, 10:33:41 AM

--- 1859-SortingtheSentence/1859-SortingtheSentence.cpp
+++ 1859-SortingtheSentence/1859-SortingtheSentence.cpp
@@ -1,19 +1,27 @@
-// Last updated: 5/16/2026, 10:21:19 AM
+// Last updated: 5/16/2026, 10:33:41 AM
 1class Solution {
 2public:
-3    int maxScore(vector<int>& nums) {
-4        sort(nums.begin(), nums.end(), greater<int>());
-5
-6        long long sum = 0;
-7        int count = 0;
-8
-9        for(int num : nums){
-10            sum += num;
-11
-12            if(sum > 0)
-13            count++;
-14        }
-15
-16        return count;
-17    }
-18};
+3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+4        int rows = matrix.size();
+5        int cols = matrix[0].size();
+6
+7        int left = 0;
+8        int right = rows * cols -1;
+9
+10        while(left <= right){
+11            int mid = left + (right - left) / 2;
+12            int value = matrix[mid/cols][mid % cols];
+13
+14            if(value == target)
+15            return true;
+16
+17            else if(value < target)
+18            left = mid + 1;
+19
+20            else 
+21                right = mid - 1;
+22        }
+23
+24        return false;
+25    }
+26};
