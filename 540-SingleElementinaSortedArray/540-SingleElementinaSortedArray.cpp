0b19714c8b5688135c9914c0ea5341433c116a6c
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-24 13:27:47 +0530

    File updated at 5/24/2026, 1:27:48 PM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.cpp
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.cpp
@@ -1,18 +1,21 @@
-// Last updated: 5/24/2026, 1:19:19 PM
+// Last updated: 5/24/2026, 1:27:48 PM
 1class Solution {
 2public:
-3    int numberOfSteps(int num) {
-4        int steps = 0;
-5
-6        while (num > 0) {
-7            if (num % 2 == 0) {
-8                num /= 2;
-9            } else {
-10                num -= 1;
-11            }
-12
-13            steps++;
-14        }
-15        return steps;
-16    }
-17};
+3    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+4        int target = k * threshold;
+5        int sum = 0;
+6        int count= 0;
+7
+8        for(int i =0; i < arr.size(); i++){
+9            sum += arr[i];
+10
+11            if(i >= k){
+12                sum -= arr[i - k];
+13            }
+14            if(i >= k -1 && sum >= target){
+15                count++;
+16            }
+17        }
+18        return count;
+19    }
+20};
