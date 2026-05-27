9d181350332582c77ef0f497dabd7661c476b265
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 15:20:29 +0530

    File updated at 5/27/2026, 3:20:29 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
@@ -1,16 +1,12 @@
-// Last updated: 5/27/2026, 3:14:51 PM
+// Last updated: 5/27/2026, 3:20:29 PM
 1class Solution {
 2public:
-3    string defangIPaddr(string address) {
-4        string ans = "";
+3    string restoreString(string s, vector<int>& indices) {
+4        string ans = s;
 5
-6        for(char c : address){
-7            if(c == '.')
-8                ans +=  "[.]";
-9
-10                else
-11                    ans += c;
-12        }
-13        return ans;
-14    }
-15};
+6        for(int i = 0; i < s.size(); i++){
+7            ans[indices[i]] = s[i];
+8        }
+9        return ans;
+10    }
+11};
