a0427fb0085feb177c26779304ecda4acdbe2910
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 14:45:18 +0530

    File updated at 5/27/2026, 2:45:18 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
@@ -1,7 +1,7 @@
-// Last updated: 5/27/2026, 2:41:31 PM
+// Last updated: 5/27/2026, 2:45:18 PM
 1class Solution {
 2public:
-3    bool isPowerOfTwo(int n) {
-4        return n > 0 && (n & (n-1)) == 0;
+3    int sum(int num1, int num2) {
+4        return num1 + num2;
 5    }
 6};
