36a1d6423ba24c3b54986bdb67be6f9b5082f4e4
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 14:12:13 +0530

    File updated at 5/27/2026, 2:12:13 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
@@ -1,12 +1,14 @@
-// Last updated: 5/27/2026, 2:04:46 PM
+// Last updated: 5/27/2026, 2:12:13 PM
 1class Solution {
 2public:
-3    int smallestEqual(vector<int>& nums) {
-4        for(int i = 0; i < nums.size(); i++){
-5            if(i % 10 == nums[i]){
-6                return i;
-7            }
-8        }
-9        return -1;
-10    }
-11};
+3    int minimumOperations(vector<int>& nums) {
+4       set<int> s;
+5
+6       for(int num : nums) {
+7        if(num > 0){
+8            s.insert(num);
+9        }
+10       }
+11       return s.size();
+12    }
+13};
