bead04dd9fa83316a3a64ce5ac461d259821babf
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 15:54:24 +0530

    File updated at 5/27/2026, 3:54:25 PM

--- 2652-SumMultiples/2652-SumMultiples.cpp
+++ 2652-SumMultiples/2652-SumMultiples.cpp
@@ -1,14 +1,7 @@
-// Last updated: 5/27/2026, 3:50:44 PM
+// Last updated: 5/27/2026, 3:54:25 PM
 1class Solution {
 2public:
-3    bool isPowerOfThree(int n) {
-4        
-5        if( n <= 0)
-6            return false;
-7
-8        while(n % 3 == 0){
-9            n /= 3;
-10        }
-11        return n == 1;
-12    }
-13};
+3    bool isSameAfterReversals(int num) {
+4        return num == 0 || num % 10 != 0;
+5    }
+6};
