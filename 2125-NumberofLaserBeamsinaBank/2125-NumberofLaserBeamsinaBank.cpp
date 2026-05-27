4e4985e6f016f4b82b7b6909965a1d2e2dee7a55
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 14:30:00 +0530

    File updated at 5/27/2026, 2:30:00 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
@@ -1,18 +1,10 @@
-// Last updated: 5/27/2026, 2:22:45 PM
+// Last updated: 5/27/2026, 2:30:00 PM
 1class Solution {
 2public:
-3    vector<vector<int>> generate(int numRows) {
-4        vector<vector <int>> triangle;
-5
-6        for(int i = 0; i < numRows; i++){
-7            vector<int> row(i + 1 , 1);
-8
-9            for(int j = 1; j < i; j++){
-10                row[j] = triangle[i-1][j-1] + triangle[i-1][j];
-11
-12            }
-13            triangle.push_back(row);
-14        }
-15        return triangle;
-16    }
-17};
+3    string toLowerCase(string s) {
+4      for(char &c : s){
+5        c = tolower(c);
+6      }  
+7      return s;
+8    }
+9};
