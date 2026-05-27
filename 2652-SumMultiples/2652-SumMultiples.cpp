8eb64b44bbeea74afa9d79f1b64f09544aec072a
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 15:43:20 +0530

    File updated at 5/27/2026, 3:43:20 PM

--- 2652-SumMultiples/2652-SumMultiples.cpp
+++ 2652-SumMultiples/2652-SumMultiples.cpp
@@ -1,8 +1,7 @@
-// Last updated: 5/27/2026, 3:39:07 PM
+// Last updated: 5/27/2026, 3:43:20 PM
 1class Solution {
 2public:
-3    vector<double> convertTemperature(double celsius) {
-4        
-5        return {celsius + 273.15, celsius * 1.80 + 32.00};
-6    }
-7};
+3    bool isPowerOfFour(int n) {
+4        return n > 0 && (n& (n-1)) == 0 && (n - 1) % 3 == 0;
+5    }
+6};
