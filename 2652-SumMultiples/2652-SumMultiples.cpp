5864447d0bd85943fb89676fd12ab86b00b6882e
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-05-27 15:39:07 +0530

    File updated at 5/27/2026, 3:39:07 PM

--- 2652-SumMultiples/2652-SumMultiples.cpp
+++ 2652-SumMultiples/2652-SumMultiples.cpp
@@ -1,15 +1,8 @@
-// Last updated: 5/27/2026, 3:35:52 PM
+// Last updated: 5/27/2026, 3:39:07 PM
 1class Solution {
 2public:
-3    int sumOfMultiples(int n) {
+3    vector<double> convertTemperature(double celsius) {
 4        
-5        int sum = 0; 
-6
-7        for(int i = 1; i <= n; i++){
-8            if(i%3 == 0 || i % 5== 0 || i % 7 == 0){
-9                sum += i;
-10            }
-11        }
-12        return sum;
-13    }
-14};
+5        return {celsius + 273.15, celsius * 1.80 + 32.00};
+6    }
+7};
