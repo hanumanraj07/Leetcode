98528a552adcc348d02860e0c327e9c5506fa119
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-07-17 15:32:36 +0530

    File updated at 7/17/2026, 3:32:35 PM

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.cpp
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.cpp
@@ -1,22 +1,24 @@
-// Last updated: 7/17/2026, 3:31:19 PM
+// Last updated: 7/17/2026, 3:32:35 PM
 1class Solution {
 2public:
 3    int findLHS(vector<int>& nums) {
-4        unordered_map<int, int> frequencyMap;
-5 
-6        for (int num : nums) {
-7            frequencyMap[num]++;
-8        }
+4
+5        sort(nums.begin(), nums.end());
+6
+7        int i = 0;
+8        int ans = 0;
 9
-10        int ans = 0;
-11 
-12        for (auto& [num, count] : frequencyMap) {
-13            if (frequencyMap.count(num + 1)) {
-14                int currentLength = count + frequencyMap[num + 1];
-15                ans = max(ans, currentLength);
-16            }
-17        }
-18
-19        return ans;
-20    }
-21};
+10        for (int j = 0; j < nums.size(); j++) {
+11
+12            while (nums[j] - nums[i] > 1) {
+13                i++;
+14            }
+15
+16            if (nums[j] - nums[i] == 1) {
+17                ans = max(ans, j - i + 1);
+18            }
+19        }
+20
+21        return ans;
+22    }
+23};
